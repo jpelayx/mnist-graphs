7f08eea1f7a4b41eb4811d2c357bac2bcd7e8a30
= <=>
2022-10-04 14:18:25 -0300

    features avg_color_distance e std_deviation_color_distance

--- mnist_slic.py
+++ mnist_slic.py
@@ -43,26 +43,35 @@ class SuperPixelGraphMNIST(InMemoryDataset):
         print(f"Average number of nodes: {self.avg_num_nodes} with standard deviation {self.std_deviation_num_nodes}")
         print(f"Average number of edges: {self.avg_num_edges} with standard deviation {self.std_deviation_num_edges}")
 
+    def select_features(self):
+        self.get_avg_color = 'avg_color' in self.features
+        if self.get_avg_color:
+            print('\t+ avg_color')
+        self.get_std_dev_color = 'std_deviation_color' in self.features
+        if self.get_std_dev_color:
+            print('\t+ std_deviation_color')
+        self.get_centroid = 'centroid' in self.features
+        if self.get_centroid:
+            print('\t+ centroid')
+        self.get_std_dev_centroid = 'std_deviation_centroid' in self.features
+        if self.get_std_dev_centroid:
+            print('\t+ std_deviation_centroid')
+        self.get_num_pixels = 'num_pixels' in self.features
+        if self.get_num_pixels:
+            print('\t+ num_pixels')
+        self.get_avg_color_distance = 'avg_color_distance' in self.features
+        if self.get_avg_color_distance:
+            print('\t+ avg_color_distance')
+        self.get_std_dev_color_distance = 'std_deviation_color_distance' in self.features
+        if self.get_std_dev_color_distance:
+            print('\t+ std_dev_color_distance')
+
     def loadMNIST(self):
         mnist = datasets.MNIST(self.root, train=self.train, download=True, transform=T.ToTensor())
         img_total = mnist.data.shape[0]
         print(f'Loading {img_total} images with n_segments = {self.n_segments} ...')
         print(f'Computing features: ')
-        self.get_avg_color = 'avg_color' in self.features
-        if self.get_avg_color:
-            print('\t+ avg_color')
-        self.get_std_deviation_color = 'std_deviation_color' in self.features
-        if self.get_std_deviation_color:
-            print('\t+ std_deviation_color')
-        self.get_centroid = 'centroid' in self.features
-        if self.get_centroid:
-            print('\t+ centroid')
-        self.get_std_deviation_centroid = 'std_deviation_centroid' in self.features
-        if self.get_std_deviation_centroid:
-            print('\t+ std_deviation_centroid')
-        self.get_num_pixels = 'num_pixels' in self.features
-        if self.get_num_pixels:
-            print('\t+ num_pixels')
+        self.select_features()
 
         t = time.time()
         data_list = [self.create_data_obj(d) for d in mnist]
@@ -94,26 +103,30 @@ class SuperPixelGraphMNIST(InMemoryDataset):
                     num_pixels[node][0] += 1
             edge_index = torch.from_numpy(np.array(g.edges).T).to(torch.long)
             x = []
-            if self.get_std_deviation_color or self.get_avg_color:
+            if self.get_std_dev_color or self.get_avg_color:
                 s1 = s1/num_pixels
                 if self.get_avg_color:
                     x.append(torch.from_numpy(s1.flatten()).to(torch.float))
-            if self.get_std_deviation_color:
+            if self.get_std_dev_color:
                 s2 = s2/num_pixels
-                std_deviation = np.sqrt(np.abs((s2 - s1*s1)))
-                x.append(torch.from_numpy(std_deviation.flatten()).to(torch.float))
+                std_dev = np.sqrt(np.abs((s2 - s1*s1)))
+                x.append(torch.from_numpy(std_dev.flatten()).to(torch.float))
             pos1 = pos1/num_pixels
             pos = torch.from_numpy(pos1).to(torch.float)
             if self.get_centroid:
                 x.append(pos[:,0])
                 x.append(pos[:,1])
-            if self.get_std_deviation_centroid:
+            if self.get_std_dev_centroid:
                 pos2 = pos2/num_pixels
-                std_deviation_centroid = torch.from_numpy(np.sqrt(np.abs(pos2 - pos1*pos1))).to(torch.float)
-                x.append(std_deviation_centroid[:,0])
-                x.append(std_deviation_centroid[:,1])
+                std_dev_centroid = torch.from_numpy(np.sqrt(np.abs(pos2 - pos1*pos1))).to(torch.float)
+                x.append(std_dev_centroid[:,0])
+                x.append(std_dev_centroid[:,1])
             if self.get_num_pixels:
                 x.append(torch.from_numpy(num_pixels.flatten()).to(torch.float))
+            if self.get_avg_color_distance:
+                x.append(torch.Tensor([np.average([g.edges[u,v]['weight'] for u,v in g.edges(node_idx)]) for node_idx in range(n)]))
+            if self.get_std_dev_color_distance:
+                x.append(torch.Tensor([np.std([g.edges[u,v]['weight'] for u,v in g.edges(node_idx)]) for node_idx in range(n)]))
             return Data(x=torch.stack(x, dim=1), edge_index=edge_index, pos=pos, y=y)
 
     def save_stats(self, data):
