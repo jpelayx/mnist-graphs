eb17d8e25d086d234b8b51d434a7b5b7e7afa223
Julia Rodrigues <j.pelayo.rodrigues@gmail.com>
2022-10-19 18:25:17 -0300

    fashion mnist tests

--- tests.sh
+++ tests.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-python model.py --features "avg_color" --dataset cifar100 --quiet --metaout cifar100
-python model.py --features "avg_color centroid" --dataset cifar100 --quiet --metaout cifar100
-python model.py --features "avg_color std_deviation_color centroid" --dataset cifar100 --quiet --metaout cifar100
-python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar100 --quiet --metaout cifar100
-python model.py --features "avg_color std_deviation_color centroid num_pixels" --dataset cifar100 --quiet --metaout cifar100
+python model.py --features "avg_color" --dataset fashion_mnist --quiet 
+python model.py --features "avg_color centroid" --dataset fashion_mnist --quiet 
+python model.py --features "avg_color std_deviation_color centroid" --dataset fashion_mnist --quiet 
+python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --quiet 
+python model.py --features "avg_color std_deviation_color centroid num_pixels" --dataset fashion_mnist --quiet 
 
-python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar100 --n_segments 10 --quiet --metaout cifar100
-python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar100 --n_segments 20 --quiet --metaout cifar100
-python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar100 --n_segments 50 --quiet --metaout cifar100
-python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar100 --n_segments 100 --quiet --metaout cifar100
-python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar100 --n_segments 200 --quiet --metaout cifar100
-python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar100 --n_segments 400 --quiet --metaout cifar100
+python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --n_segments 10 --quiet 
+python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --n_segments 20 --quiet 
+python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --n_segments 50 --quiet 
+python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --n_segments 100 --quiet 
+python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --n_segments 200 --quiet 
+python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --n_segments 400 --quiet 
