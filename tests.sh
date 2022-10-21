553cf5b37740f011bd550e073eb7d3426f874638
= <=>
2022-10-21 15:23:12 -0300

    stanfordcars experiments

--- tests.sh
+++ tests.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-python model.py --features "avg_color" --dataset stl10 --quiet 
-python model.py --features "avg_color centroid" --dataset stl10 --quiet 
-python model.py --features "avg_color std_deviation_color centroid" --dataset stl10 --quiet 
-python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stl10 --quiet 
-python model.py --features "avg_color std_deviation_color centroid num_pixels" --dataset stl10 --quiet 
+python model.py --features "avg_color" --dataset stanfordcars --quiet 
+python model.py --features "avg_color centroid" --dataset stanfordcars --quiet 
+python model.py --features "avg_color std_deviation_color centroid" --dataset stanfordcars --quiet 
+python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stanfordcars --quiet 
+python model.py --features "avg_color std_deviation_color centroid num_pixels" --dataset stanfordcars --quiet 
 
-python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stl10 --n_segments 10 --quiet 
-python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stl10 --n_segments 20 --quiet 
-python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stl10 --n_segments 50 --quiet 
-python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stl10 --n_segments 100 --quiet 
-python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stl10 --n_segments 200 --quiet 
-python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stl10 --n_segments 400 --quiet 
+python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stanfordcars --n_segments 10 --quiet 
+python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stanfordcars --n_segments 20 --quiet 
+python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stanfordcars --n_segments 50 --quiet 
+python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stanfordcars --n_segments 100 --quiet 
+python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stanfordcars --n_segments 200 --quiet 
+python model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stanfordcars --n_segments 400 --quiet 
