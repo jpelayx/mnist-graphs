5fb68f2dfd684f7592b93f0b6311c51590ba034c
Julia Rodrigues <j.pelayo.rodrigues@gmail.com>
2022-11-29 15:51:52 -0300

    added color datasets to tests

--- tests.sh
+++ tests.sh
@@ -1,22 +1,55 @@
 #!/bin/sh
-python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset mnist --graph_type 1NNSpatial --slic_method SLIC0 
-python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset mnist --graph_type 2NNSpatial --slic_method SLIC0 
-python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset mnist --graph_type 4NNSpatial --slic_method SLIC0 
-python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset mnist --graph_type 8NNSpatial --slic_method SLIC0 
-python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset mnist --graph_type 16NNSpatial --slic_method SLIC0 
-python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset mnist --graph_type 1NNFeature --slic_method SLIC0 
-python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset mnist --graph_type 2NNFeature --slic_method SLIC0 
-python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset mnist --graph_type 4NNFeature --slic_method SLIC0 
-python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset mnist --graph_type 8NNFeature --slic_method SLIC0 
-python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset mnist --graph_type 16NNFeature --slic_method SLIC0 
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset mnist --graph_type 1NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset mnist --graph_type 2NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset mnist --graph_type 4NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset mnist --graph_type 8NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset mnist --graph_type 16NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset mnist --graph_type 1NNFeature --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset mnist --graph_type 2NNFeature --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset mnist --graph_type 4NNFeature --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset mnist --graph_type 8NNFeature --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset mnist --graph_type 16NNFeature --slic_method SLIC0  --quiet
 
-python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --graph_type 1NNSpatial --slic_method SLIC0 
-python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --graph_type 2NNSpatial --slic_method SLIC0 
-python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --graph_type 4NNSpatial --slic_method SLIC0 
-python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --graph_type 8NNSpatial --slic_method SLIC0 
-python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --graph_type 16NNSpatial --slic_method SLIC0 
-python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --graph_type 1NNFeature --slic_method SLIC0 
-python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --graph_type 2NNFeature --slic_method SLIC0 
-python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --graph_type 4NNFeature --slic_method SLIC0 
-python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --graph_type 8NNFeature --slic_method SLIC0 
-python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --graph_type 16NNFeature --slic_method SLIC0 
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --graph_type 1NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --graph_type 2NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --graph_type 4NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --graph_type 8NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --graph_type 16NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --graph_type 1NNFeature --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --graph_type 2NNFeature --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --graph_type 4NNFeature --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --graph_type 8NNFeature --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset fashion_mnist --graph_type 16NNFeature --slic_method SLIC0  --quiet
+
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar10 --graph_type 1NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar10 --graph_type 2NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar10 --graph_type 4NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar10 --graph_type 8NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar10 --graph_type 16NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar10 --graph_type 1NNFeature --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar10 --graph_type 2NNFeature --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar10 --graph_type 4NNFeature --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar10 --graph_type 8NNFeature --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar10 --graph_type 16NNFeature --slic_method SLIC0  --quiet
+
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar100 --graph_type 1NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar100 --graph_type 2NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar100 --graph_type 4NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar100 --graph_type 8NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar100 --graph_type 16NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar100 --graph_type 1NNFeature --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar100 --graph_type 2NNFeature --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar100 --graph_type 4NNFeature --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar100 --graph_type 8NNFeature --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset cifar100 --graph_type 16NNFeature --slic_method SLIC0  --quiet
+
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stl10 --graph_type 1NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stl10 --graph_type 2NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stl10 --graph_type 4NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stl10 --graph_type 8NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stl10 --graph_type 16NNSpatial --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stl10 --graph_type 1NNFeature --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stl10 --graph_type 2NNFeature --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stl10 --graph_type 4NNFeature --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stl10 --graph_type 8NNFeature --slic_method SLIC0  --quiet
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset stl10 --graph_type 16NNFeature --slic_method SLIC0  --quiet
