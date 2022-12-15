60bdf2a3b6827541f807f4236990b4f579f4e662
Julia Rodrigues <j.pelayo.rodrigues@gmail.com>
2022-12-15 16:41:30 -0300

    loading and testing all n_segments variations with SLICO

--- load_geo_ds.sh
+++ load_geo_ds.sh
@@ -4,3 +4,39 @@ python3 load_dataset.py --features "avg_color std_deviation_color centroid std_d
 python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  20 --slic_method SLIC0
 python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  20 --slic_method SLIC0
 python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  20 --slic_method SLIC0
+
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  10 --slic_method SLIC0  --quiet
+
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  20 --slic_method SLIC0  --quiet
+
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  50 --slic_method SLIC0
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  50 --slic_method SLIC0
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  50 --slic_method SLIC0
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  50 --slic_method SLIC0
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  50 --slic_method SLIC0
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  50 --slic_method SLIC0
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  50 --slic_method SLIC0  --quiet
+
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  100 --slic_method SLIC0
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  100 --slic_method SLIC0
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  100 --slic_method SLIC0
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  100 --slic_method SLIC0
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  100 --slic_method SLIC0
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  100 --slic_method SLIC0
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments 100 --slic_method SLIC0  --quiet
+
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  200 --slic_method SLIC0
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  200 --slic_method SLIC0
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  200 --slic_method SLIC0
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  200 --slic_method SLIC0
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  200 --slic_method SLIC0
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  200 --slic_method SLIC0
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments 200 --slic_method SLIC0  --quiet
+
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  400 --slic_method SLIC0
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  400 --slic_method SLIC0
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  400 --slic_method SLIC0
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  400 --slic_method SLIC0
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  400 --slic_method SLIC0
+python3 load_dataset.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments  400 --slic_method SLIC0
+python3 model.py --features "avg_color std_deviation_color centroid std_deviation_centroid" --dataset geo_ds --graph_type RAG --n_segments 400 --slic_method SLIC0  --quiet
