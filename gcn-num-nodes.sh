82390684b010556c98d0cc8cfa0d66ebedb46a49
Julia Rodrigues <j.pelayo.rodrigues@gmail.com>
2023-07-25 16:57:18 -0300

    added partial run of GCN num. nodes experiment

--- gcn-num-nodes.sh
+++ gcn-num-nodes.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
-python3 train-model.py --dataset mnist -f GCN/mnist-nodes --n_segments 10  
-python3 train-model.py --dataset mnist -f GCN/mnist-nodes --n_segments 20  
-python3 train-model.py --dataset mnist -f GCN/mnist-nodes --n_segments 50  
-python3 train-model.py --dataset mnist -f GCN/mnist-nodes --n_segments 100  
-python3 train-model.py --dataset mnist -f GCN/mnist-nodes --n_segments 200  
-python3 train-model.py --dataset mnist -f GCN/mnist-nodes --n_segments 400  
-
-python3 train-model.py --dataset fashion_mnist -f GCN/fashion_mnist-nodes --n_segments 10  
-python3 train-model.py --dataset fashion_mnist -f GCN/fashion_mnist-nodes --n_segments 20  
-python3 train-model.py --dataset fashion_mnist -f GCN/fashion_mnist-nodes --n_segments 50  
-python3 train-model.py --dataset fashion_mnist -f GCN/fashion_mnist-nodes --n_segments 100  
-python3 train-model.py --dataset fashion_mnist -f GCN/fashion_mnist-nodes --n_segments 200  
-python3 train-model.py --dataset fashion_mnist -f GCN/fashion_mnist-nodes --n_segments 400  
-
-python3 train-model.py --dataset cifar10 -f GCN/cifar10-nodes --n_segments 10  
-python3 train-model.py --dataset cifar10 -f GCN/cifar10-nodes --n_segments 20  
-python3 train-model.py --dataset cifar10 -f GCN/cifar10-nodes --n_segments 50  
-python3 train-model.py --dataset cifar10 -f GCN/cifar10-nodes --n_segments 100  
-python3 train-model.py --dataset cifar10 -f GCN/cifar10-nodes --n_segments 200  
-python3 train-model.py --dataset cifar10 -f GCN/cifar10-nodes --n_segments 400  
-
-python3 train-model.py --dataset cifar100 -f GCN/cifar100-nodes --n_segments 10  
+# python3 train-model.py --dataset mnist -f GCN/mnist-nodes --n_segments 10  
+# python3 train-model.py --dataset mnist -f GCN/mnist-nodes --n_segments 20  
+# python3 train-model.py --dataset mnist -f GCN/mnist-nodes --n_segments 50  
+# python3 train-model.py --dataset mnist -f GCN/mnist-nodes --n_segments 100  
+# python3 train-model.py --dataset mnist -f GCN/mnist-nodes --n_segments 200  
+# python3 train-model.py --dataset mnist -f GCN/mnist-nodes --n_segments 400  
+# 
+# python3 train-model.py --dataset fashion_mnist -f GCN/fashion_mnist-nodes --n_segments 10  
+# python3 train-model.py --dataset fashion_mnist -f GCN/fashion_mnist-nodes --n_segments 20  
+# python3 train-model.py --dataset fashion_mnist -f GCN/fashion_mnist-nodes --n_segments 50  
+# python3 train-model.py --dataset fashion_mnist -f GCN/fashion_mnist-nodes --n_segments 100  
+# python3 train-model.py --dataset fashion_mnist -f GCN/fashion_mnist-nodes --n_segments 200  
+# python3 train-model.py --dataset fashion_mnist -f GCN/fashion_mnist-nodes --n_segments 400  
+# 
+# python3 train-model.py --dataset cifar10 -f GCN/cifar10-nodes --n_segments 10  
+# python3 train-model.py --dataset cifar10 -f GCN/cifar10-nodes --n_segments 20  
+# python3 train-model.py --dataset cifar10 -f GCN/cifar10-nodes --n_segments 50  
+# python3 train-model.py --dataset cifar10 -f GCN/cifar10-nodes --n_segments 100  
+# python3 train-model.py --dataset cifar10 -f GCN/cifar10-nodes --n_segments 200  
+# python3 train-model.py --dataset cifar10 -f GCN/cifar10-nodes --n_segments 400  
+# 
+# python3 train-model.py --dataset cifar100 -f GCN/cifar100-nodes --n_segments 10  
 python3 train-model.py --dataset cifar100 -f GCN/cifar100-nodes --n_segments 20  
 python3 train-model.py --dataset cifar100 -f GCN/cifar100-nodes --n_segments 50  
 python3 train-model.py --dataset cifar100 -f GCN/cifar100-nodes --n_segments 100  
