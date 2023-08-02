da8986c87402ab8075caf64d934632855b33ded9
Julia Rodrigues <j.pelayo.rodrigues@gmail.com>
2023-08-02 09:41:37 -0300

    added new validation set size experiments

--- validation-set-size.sh
+++ validation-set-size.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-python3 train-model.py -m GAT --dataset stl10 -f GAT/stl10-validation-size --n_layers 2 --validation_size 0.20 
-python3 train-model.py -m GAT --dataset stl10 -f GAT/stl10-validation-size --n_layers 2 --validation_size 0.20
-python3 train-model.py -m GAT --dataset stl10 -f GAT/stl10-validation-size --n_layers 3 --validation_size 0.20
-python3 train-model.py -m GAT --dataset stl10 -f GAT/stl10-validation-size --n_layers 4 --validation_size 0.20
-python3 train-model.py -m GAT --dataset stl10 -f GAT/stl10-validation-size --n_layers 5 --validation_size 0.20
+python3 train-model.py -m GAT --dataset cifar10 -f GAT/cifar10-validation-size --n_heads  1 --validation_size 0.25 
+python3 train-model.py -m GAT --dataset cifar10 -f GAT/cifar10-validation-size --n_heads  2 --validation_size 0.25
+python3 train-model.py -m GAT --dataset cifar10 -f GAT/cifar10-validation-size --n_heads  4 --validation_size 0.25
+python3 train-model.py -m GAT --dataset cifar10 -f GAT/cifar10-validation-size --n_heads  8 --validation_size 0.25
+python3 train-model.py -m GAT --dataset cifar10 -f GAT/cifar10-validation-size --n_heads 16 --validation_size 0.25
