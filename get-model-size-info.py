353d609900c8ab9d790fcc2b1533b20cab040eec
Julia Rodrigues <j.pelayo.rodrigues@gmail.com>
2023-10-09 13:13:39 -0300

    model size information for best performing GCN models and AlexNet

--- get-model-size-info.py
+++ get-model-size-info.py
@@ -13,26 +13,34 @@ if __name__ == '__main__':
         'stl10'
     ]
 
-    num_heads = [1, 2, 4, 8, 16]
+    ids = ['best model']
+    basedir = 'AlexNet'
+    model_paths = {
+        'mnist': ['AlexNet-lr1e-05'],
+        'fashion_mnist': ['AlexNet-lr1e-05'],
+        'cifar10': ['AlexNet-lr1e-05'],
+        'cifar100': ['AlexNet-lr1e-05'],
+        'stl10': ['AlexNet-lr1e-05']
+    }
 
     out_fields = [
-        'num. of heads', 
+        'model', 
         'model size'
     ]
 
 
     for ds in dss:
-        basedir = f'GAT/{ds}/'
-        out_file = f'GAT/{ds}-heads-modelsizeinfo.csv'
+        out_file = basedir + f'/{ds}-comparisons-modelsizeinfo.csv'
         with open(out_file, 'w', newline='') as file:
             writer = csv.DictWriter(file, fieldnames=out_fields)
             writer.writeheader()
         sizes = []
         print(ds)
-        for h in num_heads:
+        for id, path in enumerate(model_paths[ds]):
+            print('Filename: ', path)
             fold_sizes = []
             for f in range(5):
-                model_path = basedir + f'l4h{h}n75-RAG-SLIC0-avg_color-centroid-std_deviation_centroid-std_deviation_color.fold{f}.pth'
+                model_path = basedir + f'/{ds}/' + path + f'.fold{f}.pth'
                 model = torch.load(model_path)
                 fold_size = 0
                 for layer in model:
@@ -41,10 +49,10 @@ if __name__ == '__main__':
                 fold_sizes.append(fold_size)
             mean_size = np.mean(fold_sizes)
             sizes.append(mean_size)
-            print(h, mean_size)
+            print(ids[id], mean_size)
             with open(out_file, 'a', newline='') as file:
                 writer = csv.DictWriter(file, fieldnames=out_fields)
-                writer.writerow({'num. of heads': h, 'model size': mean_size})
+                writer.writerow({out_fields[0] : ids[id], out_fields[1] : mean_size})
 
         
 
